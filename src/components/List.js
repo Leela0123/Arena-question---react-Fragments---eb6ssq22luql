fee7d082d567df467ed45360c8c07dd022124c41
Leela0123 <117524740+Leela0123@users.noreply.github.com>
2023-05-14 08:11:48 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ListItems from './ListItems';
 
- const List = ({listx}) =>{
-  return(
-    // code here
-    
-      <ListItems  />
-    
-    
-  )
-}
+const List = ({ listx }) => {
+  return (
+    <>
+      {listx.map((item) => (
+        <ListItems key={item} valuex={item} />
+      ))}
+    </>
+  );
+};
 
 export default List;
